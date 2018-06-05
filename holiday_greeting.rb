7bed33f906f2525b2402657795523cb8387360ad
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-05 04:39:22 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,17 @@
+#CLI 
+ 
+  puts "Welcome to my holiday greeting program. What is your name?"
+  from = gets.strip
+  
+  puts "Who do you want to send a holiday greeting to?"
+  to = gets.strip
+  
+  puts "What is the holiday that you wish to celebrate?"
+  holiday = gets.strip
+
+def holiday_greeting(to, from, holiday)
+  "Happy #{holiday}, #{to}! - From #{from}"
+end 
+
+holiday_greeting(to, from, holiday)
+
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-
+def mothers_day(name = "Mom")
+  puts "Happy mothers day, #{name}! I hope you have a great day!"
+end
 
 ## Call your method below - use puts to see it in the terminal!
 
+puts mothers_day("Tracy")
