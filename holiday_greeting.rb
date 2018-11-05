431061c22e20041c31dd0ebc2eac20773d9ccae9
Mehreen Butt <mbutt@republiccharterschools.org>
2018-11-05 21:19:31 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,17 +1,16 @@
-#CLI 
- 
+#CLI
+
   puts "Welcome to my holiday greeting program. What is your name?"
   from = gets.strip
-  
+
   puts "Who do you want to send a holiday greeting to?"
   to = gets.strip
-  
+
   puts "What is the holiday that you wish to celebrate?"
   holiday = gets.strip
 
 def holiday_greeting(to, from, holiday)
-  puts "Happy #{holiday}, #{to}! - From #{from}"
-end 
+  "Happy #{holiday}, #{to}! - From #{from}"
+end
 
 holiday_greeting(to, from, holiday)
-
